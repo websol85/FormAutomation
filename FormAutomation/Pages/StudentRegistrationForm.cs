938f870e5ad6db88634ae12b4d60d47ac70e4c57
Hammad Farooq <hammad.farooq@LHI.local>
2021-04-06 19:03:32 +0500

    Add project files.

---
+++ FormAutomation/Pages/StudentRegistrationForm.cs
@@ -0,0 +1,92 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace FormAutomation.Pages
+{
+    class StudentRegistrationForm
+    {
+        [Obsolete]
+        public StudentRegistrationForm(IWebDriver Chromedriver)
+        {
+            driver = Chromedriver;
+            PageFactory.InitElements(Chromedriver, this);
+        }
+        private static IWebDriver driver;
+
+
+
+        [FindsBy(How = How.Id, Using = "firstName")]
+        private IWebElement fNameTxt { get; set; }
+        [FindsBy(How = How.Id, Using = "lastName")]
+        private IWebElement lNameTxt { get; set; }
+        [FindsBy(How = How.XPath, Using = "//*[@id='genterWrapper']/div[2]/div[1]/label")]
+        private IWebElement genderRadioBtn { get; set; }
+        [FindsBy(How = How.Id, Using = "userNumber")]
+        private IWebElement mobileTxt { get; set; }
+        [FindsBy(How = How.XPath, Using = "//*[@id='submit']")]
+        private IWebElement btnSubmit { get; set; }
+        [FindsBy(How = How.Id, Using = "example-modal-sizes-title-lg")]
+        private IWebElement divThanks { get; set; }
+
+
+
+
+
+
+        public void ExplicitWait(By element)
+        {
+
+
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(element));
+        }
+        public bool isElementExist(IWebElement element)
+        {
+
+            return element.Displayed && element.Enabled;
+        }
+
+
+
+        public void WriteText(IWebElement element, String text)
+        {
+            element.Clear();
+            element.SendKeys(text);
+        }
+
+
+
+        public void BtnClick(IWebElement btn)
+        {
+            btn.Click();
+        }
+
+
+
+        public bool SubmitForm()
+        {
+            ExplicitWait(By.Id("firstName"));
+            WriteText(fNameTxt, "FName");
+            WriteText(lNameTxt, "LName");
+            BtnClick(genderRadioBtn);
+            WriteText(mobileTxt, "033335347762");
+            BtnClick(btnSubmit);
+            ExplicitWait(By.Id("example-modal-sizes-title-lg"));
+            return isElementExist(divThanks);
+        }
+
+
+
+
+    }
+}
---
+++ FormAutomation/Program.cs
@@ -0,0 +1,50 @@
+﻿using FormAutomation.Pages;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace FormAutomation
+{
+    class Program
+    {
+        IWebDriver driver = new ChromeDriver();
+        StudentRegistrationForm objStudent = null;
+        static void Main(string[] args)
+        {
+        }
+
+
+
+        [SetUp]
+        public void Initialize()
+        {
+            driver.Navigate().GoToUrl("https://demoqa.com/automation-practice-form");
+            driver.Manage().Window.Maximize();
+            objStudent = new StudentRegistrationForm(driver);
+
+        }
+
+
+
+        [Test]
+        public void ExecuteTest()
+        {
+            objStudent.SubmitForm();
+        }
+
+
+
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Close();
+        }
+    }
+}
